aa221ae301b727e62307d81ff46fedbaf75e0510
Yang Xiao <yangyan1@live.unc.edu>
2018-03-04 13:27:59 -0500

    1st
    1stversion ar project

---
+++ 585Project/Assets/script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("yoyoyo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
