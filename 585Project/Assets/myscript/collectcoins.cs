8385f5d8004b00912908093809cde93bdea39637
Yang Xiao <yangyan1@live.unc.edu>
2018-03-22 15:48:26 -0400

    v2

---
+++ 585Project/Assets/myscript/collectcoins.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectcoins : MonoBehaviour {
+	public GameObject ccoin;
+	public int scorecount = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("s")){
+			Destroy(ccoin);
+			scorecount++;
+		}
+	}
+}
