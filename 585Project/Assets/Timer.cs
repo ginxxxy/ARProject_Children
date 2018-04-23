39a7119e33f9827ffe68e91b41659bab3aa07c27
Emily Barnard <em.barnard.shao@gmail.com>
2018-04-22 20:19:12 -0400

    Timer and Pause

--- 585Project/Assets/Timer.cs
+++ 585Project/Assets/Timer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
 
 	public Text timerText;
 	public float startTime;
-	public bool gamePaused = true;
+	public bool gamePaused;
+	Scene currentScene;
+	string sceneName;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +19,9 @@ public class Timer : MonoBehaviour {
 			startTime = Time.time;
 		}
 		Debug.Log ("STATE of Pause: " + gamePaused);
+
+		currentScene = SceneManager.GetActiveScene ();
+		sceneName = currentScene.name;
 	}
 
 	// Update is called once per frame
@@ -24,18 +30,15 @@ public class Timer : MonoBehaviour {
 		string minutes = ((int)t / 60).ToString ();
 		string seconds = (t % 60).ToString ("f0");
 		timerText.text = minutes + ":" + seconds;
-		if (!gamePaused) {
-			t += Time.deltaTime;
+
+		if (sceneName == "mainMenu" || sceneName == "starRating") {
+			gamePaused = true;
+			Time.timeScale = 0f;
+			Debug.Log ("PAUSED");
+		} else {
+			gamePaused = false;
+			Time.timeScale = 1f;
+			Debug.Log ("UNPAUSED");
 		}
-	}
-
-	public void pauseGame(){
-		gamePaused = true;
-		Debug.Log ("PAUSED");
-	}
-
-	public void unpauseGame(){
-		gamePaused = false;
-		Debug.Log ("UNPAUSED");
 	}
 }
